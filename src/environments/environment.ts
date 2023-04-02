806e96d9392b1986b5bc8cd5d6527eac6f8d6bdd
watydiaz <40471433+watydiaz@users.noreply.github.com>
2023-04-02 11:56:57 -0500

    Copiando datos de firebase  en environmets

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDCmrMhksYqjSghcobrU8qJHRApX5MUgL4",
+    authDomain: "crud-incidencias.firebaseapp.com",
+    projectId: "crud-incidencias",
+    storageBucket: "crud-incidencias.appspot.com",
+    messagingSenderId: "900880523164",
+    appId: "1:900880523164:web:584e3eaaa8c0fefbfcb259"
+  }
 };
 
 /*
